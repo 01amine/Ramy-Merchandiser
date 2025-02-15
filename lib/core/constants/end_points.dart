a569a61114b33f02b46ab69e38d25c6574d158e1
its0amine <mohamedamine.heddouche@etu.usthb.dz>
2025-02-15 01:42:32 +0100

    linked with the backend

---
+++ lib/core/constants/end_points.dart
@@ -0,0 +1,8 @@
+class EndPoints {
+  static const String BASE_URL = "http://localhost:3000/";
+  static  const String login = "{$BASE_URL}login";
+  static  const String register = "{$BASE_URL}register";
+  static  const String scan = "{$BASE_URL}scan";
+  static const String profile = "{$BASE_URL}profile";
+  static const String dashboard = "{$BASE_URL}dashboard";
+}
